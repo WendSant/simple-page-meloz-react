918c3b1ef3bf3ee85dfd5da70b3d01a259c5d6c4
WendSant <wendsonsantanajps@gmail.com>
2022-08-23 00:10:03 -0300

    Created Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>testes</h1>
+    <div>
+      <Home></Home>
     </div>
   );
 }
