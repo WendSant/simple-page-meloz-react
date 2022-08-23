5f24a5c8abf3dbe950050029dae57c1ddf1dabbf
WendSant <wendsonsantanajps@gmail.com>
2022-08-23 17:27:06 -0300

    navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import { Container, NavButton } from "./style";
+
+const NavBar = () => {
+    return (
+        <Container>
+            <NavButton href="#">Lista de produtos</NavButton>
+            <NavButton href="#">Criar produto</NavButton>
+            <NavButton href="#">Editar produto</NavButton>
+        </Container>
+    )
+}
+
+export default NavBar;
---
+++ src/components/NavBar/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+export const NavButton = styled.a`
+    text-decoration: none;
+    color: black;
+    padding: 10px;
+`
