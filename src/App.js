75dd7796730ca3d121d81cd948d7973535efd291
WendSant <wendsonsantanajps@gmail.com>
2022-08-23 17:27:37 -0300

    add navbar in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Home></Home>
     </div>
   );
