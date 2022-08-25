ef42e77bbdbdb152dab91487574b5bc764d6cdf3
WendSant <wendsonsantanajps@gmail.com>
2022-08-25 00:07:11 -0300

    edit product

---
+++ src/pages/EditProduct/index.js
@@ -0,0 +1,12 @@
+import { Container } from "./style";
+
+
+const EditProduct= () => {
+    return(
+        <Container>
+            Edit Product
+        </Container>
+    )
+}
+
+export default EditProduct;
