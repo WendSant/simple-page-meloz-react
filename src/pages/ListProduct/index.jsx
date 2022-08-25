4f2b2d7bbf2032505c460479a57ad8a511567725
WendSant <wendsonsantanajps@gmail.com>
2022-08-24 23:52:09 -0300

    list product

---
+++ src/pages/ListProduct/index.jsx
@@ -0,0 +1,12 @@
+
+import { Container } from "./style";
+
+const ListProduct = () => {
+    return (
+        <Container>
+            List Product
+        </Container>
+    );
+};
+
+export default ListProduct;
