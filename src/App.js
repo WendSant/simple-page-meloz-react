a74b65515e60d67d4df533d8161b9e940623e98f
WendSant <wendsonsantanajps@gmail.com>
2022-09-01 19:42:28 -0300

    useState e começo de props

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import EditProduct from './pages/EditProduct'
 import Home from './pages/Home';
 import CreateProduct from './pages/CreateProduct';
 import { Container } from './style';
+import { useState } from 'react';
 
 function App() {
+
+  const PAGE_HOME = 1;
+  const PAGE_CREATE= 2;
+  const PAGE_EDIT = 3;
+  const PAGE_LIST= 4;
+
+  const [atualPage, setAtualPage] = useState(PAGE_HOME);
+
   return (
     <Container>
       <NavBar/>
-      <Home/>
-      <ListProduct/>
-      <EditProduct/>
-      <CreateProduct/>
+      {
+        atualPage === PAGE_HOME && <Home />
+      }
+      {
+        atualPage === PAGE_CREATE ? <CreateProduct/> : <></>
+      }
+      {
+        atualPage === PAGE_EDIT && <EditProduct/>
+      }
+      {
+        atualPage === PAGE_LIST && <ListProduct/>
+      }
     </Container>
   );
 }
