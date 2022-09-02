51ba5a675ab664d8c3a3472d5e1d7e3dbbbc956c
WendSant <wendsonsantanajps@gmail.com>
2022-09-02 20:35:25 -0300

    produtos adicionados

--- src/support/constants.js
+++ src/support/constants.js
@@ -4,3 +4,21 @@ export const PAGE_HOME = 1;
 export const PAGE_CREATE= 2;
 export const PAGE_EDIT = 3;
 export const PAGE_LIST= 4;
+
+export const initialProducts = [
+    {
+        id: 1,
+        name: 'produto 1',
+        price: 30.00
+    },
+    {
+        id: 2,
+        name: 'produto 2',
+        price: 20.00
+    },
+    {
+        id: 3,
+        name: 'produto 3',
+        price: 10.00
+    }
+]
